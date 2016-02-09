2f12e7dcebbaa7dc405732a2d08ccad466099958
Alexander Kryklia <kryklia@gmail.com>
2016-02-09 12:46:39 +0200

    Add models and timezone awareness.

--- gaming/admin.py
+++ gaming/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
-# Register your models here.
+from gaming.models import Game, Team, UserTeam, UserRequest
+
+class GameAdmin(admin.ModelAdmin):
+    pass
+class TeamAdmin(admin.ModelAdmin):
+    pass
+class UserTeamAdmin(admin.ModelAdmin):
+    pass
+class UserRequestAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(Game, GameAdmin)
+admin.site.register(Team, TeamAdmin)
+admin.site.register(UserTeam, UserTeamAdmin)
+admin.site.register(UserRequest, UserRequestAdmin)
--- gaming/models.py
+++ gaming/models.py
@@ -1,5 +1,37 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.conf import settings
+from django.utils import timezone
 
-# Create your models here.
+class Game(models.Model):
+    """Game entity"""
+    name = models.CharField(blank=False, max_length=255)
+    teamsize = models.PositiveIntegerField(blank=False, null=False)
+
+class Team(models.Model):
+    """Teams"""
+    name = models.CharField(blank=False, max_length=100)
+
+class UserTeam(models.Model):
+    """Links users and Teams"""
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+    )
+    team = models.ForeignKey(Team)
+
+class UserRequest(models.Model):
+    """
+    Requests of Games in time.
+
+    TODO: enable timezone support via userprofiles
+    https://docs.djangoproject.com/en/1.9/topics/i18n/timezones/
+    """
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+    )
+    game = models.ForeignKey(Game)
+    start = models.DateTimeField(blank=False, default=timezone.now)
+    end = models.DateTimeField(blank=False, default=timezone.now)
